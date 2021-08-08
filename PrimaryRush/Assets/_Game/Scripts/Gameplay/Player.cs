9c4c49c1097b418c76af36f23f70e8a3cc2a1f99
kristenmc <kriscampbell1999@gmail.com>
2021-08-08 15:12:07 -0700

    Removed Unity Audio Sources

--- PrimaryRush/Assets/_Game/Scripts/Gameplay/Player.cs
+++ PrimaryRush/Assets/_Game/Scripts/Gameplay/Player.cs
@@ -30,7 +30,6 @@ public class Player : MonoBehaviour
     //12.26
 
     private AudioSource AS;
-   public  AudioSource AS2;
 
     //FMOD SFX Instances (For sounds that need to be dynamically handled)
     private FMOD.Studio.EventInstance PlaySlowMoSound;
@@ -245,7 +244,6 @@ public class Player : MonoBehaviour
     }
     
     private void Die() {
-        AS2.Stop();
         info.alive = false;
         particle.startColor = UnityEngine.Color.white;
         particle.Play();
